a8c0f013f26990321b42fa7a9954997bfe72c77c
Jordan Newhouse <jkimys2@gmail.com>
2024-04-06 12:09:50 -0400

    webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,60 @@ module.exports = () => {
       path: path.resolve(__dirname, "dist"),
     },
     plugins: [
+      // Service Worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
 
+      // Webpack plugin that generates the html file and injects bundles
+      new HtmlWebpackPlugin({
+        template: "index.html",
+        title: "J.A.T.E",
+      }),
+
+      // Creates a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "J.A.T.E",
+        description: "Just Another Text Editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        public_path: "./",
+        icons: [
+          {
+            src: path.resolve("src/favicon.ico"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-
+        // CSS loaders
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel loader
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
