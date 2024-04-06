be5eeb70970fd6c192f6ddb84ce099e546ec505c
Jordan Newhouse <jkimys2@gmail.com>
2024-04-06 16:12:04 -0400

    files update

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -28,7 +28,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute( 
-  // Define the callback function that will filter the requests we want to cache
+  // Define the callback function that will filter the requests we want to cache 
 ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
 new StaleWhileRevalidate({
   // Name of the cache storage.
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,8 +14,6 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error("putDb not implemented");
-
   // Create connection to database and the database version you want to use
   const jateDb = await openDB("jate", 1);
 
@@ -26,15 +24,13 @@ export const putDb = async (content) => {
   const store = tx.objectStore("jate");
 
   // Get confirmation of the request
-  const request = store.put({ content: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log("Data saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error("getDb not implemented");
-
   // Create connection to database and the database version you want to use
   const jateDb = await openDB("jate", 1);
 
@@ -45,12 +41,12 @@ export const getDb = async () => {
   const store = tx.objectStore("jate");
 
   // Use the .getAll() method to get all data in the database
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log("result.value", result);
-  return result;
+  return result.value;
 };
 
 initdb();
